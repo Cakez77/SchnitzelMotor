c86bc865ddda8ceb967d6ca1042dce2064335145
Cakez77 <bernd.eissing@gmail.com>
2023-07-10 12:45:16 +0200

    Changed build script to work on linux

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ if [[ "$(uname)" == "Linux" ]]; then
     libs="-lX11 -lGL"
     includes="-Ithird_party"
     outputFile=schnitzel
-    queryProcesses=$(echo "ps ax")
+    queryProcesses=$(echo pgrep $outputFile)
 
 elif [[ "$(uname)" == "Darwin" ]]; then
     echo "Running on Mac"
@@ -23,7 +23,7 @@ else
     libs="-luser32 -lgdi32 -lopengl32"
     includes="-Ithird_party"
     outputFile=schnitzel.exe
-    queryProcesses=$(echo "tasklist")
+    queryProcesses=$(echo "tasklist" grep $outputFile)
 
     timestamp=$(date +%s)
     rm -f game_* # Remove old game_* files
@@ -31,10 +31,12 @@ else
     mv game_$timestamp.dll game.dll
 fi
 
-processRunning=$($queryProcesses | grep $outputFile)
+processRunning=$($queryProcesses)
 
 if [ -z "$processRunning" ]; then
-    echo "Process not running, building Engine"
+    echo "Engine not running, building main..."
     clang++ $includes $cflags -g "src/main.cpp" $objc_dep -o $outputFile $libs $warnings
+else
+    echo "Engine running"
 fi
 
