0f962d219dfab631712fca8521d508c0044e23da
Cakez77 <bernd.eissing@gmail.com>
2023-07-11 15:26:36 +0200

    added a first version of jumping

--- src/game.cpp
+++ src/game.cpp
@@ -12,7 +12,6 @@ EXPORT_FN void update_game(GameState* gameStateIn, Input* inputIn, RenderData* r
     input = inputIn;
     renderData = renderDataIn;
     gameState = gameStateIn;
-    gameState->playerPos = {};
   }
 
   // This is on the stack
@@ -22,12 +21,19 @@ EXPORT_FN void update_game(GameState* gameStateIn, Input* inputIn, RenderData* r
   float dt = 1.0f / 60.0f;
   float maxRunSpeed = 10.0f;
   float runAcceleration = 100.0f;
-  float maxJumpSpeed = 40.0f;
-  float jumpAcceleration = 1200.0f;
-  float jumpReduce = 80.0f;
-  float runReduce =  40.0f;
+  float maxJumpSpeed = -20.0f;
+  float fallSpeed = 20.0f;
+  float gravity = 90.0f;
+  float runReduce = 40.0f;
   static Vec2 speed;
   static bool grounded = true;
+  static float highestHeight = input->screenSize.y;
+  static float varJumpTimer = 0.0f;
+
+  if(key_pressed_this_frame(KEY_R))
+  {
+    gameState->playerPos = {};
+  }
 
   if(key_is_down(KEY_A))
   {
@@ -39,22 +45,22 @@ EXPORT_FN void update_game(GameState* gameStateIn, Input* inputIn, RenderData* r
     speed.x = approach(speed.x, maxRunSpeed, runAcceleration * dt);
   }
 
-  if(key_is_down(KEY_SPACE))
-  {
-    speed.y = approach(speed.y, -maxJumpSpeed, 0.4f + dt);
-  }
-
   if(key_pressed_this_frame(KEY_SPACE) &&
      grounded)
   {
-    speed.y = approach(speed.y, -maxJumpSpeed, jumpAcceleration * dt);
+    varJumpTimer = 0.0f;
+    speed.y = maxJumpSpeed;
     grounded = false;
   }
-  else
-  {
-    speed.y = approach(speed.y, 0, jumpReduce * dt);
-  }
 
+  if(key_is_down(KEY_SPACE) &&
+     varJumpTimer < 0.1f)
+  {
+    speed.y = min(speed.y, maxJumpSpeed);
+  }
+  varJumpTimer += dt;
+
+  speed.y = approach(speed.y, fallSpeed, gravity * dt);
 
   // firction
   if(!key_is_down(KEY_A) &&
@@ -76,15 +82,23 @@ EXPORT_FN void update_game(GameState* gameStateIn, Input* inputIn, RenderData* r
   gameState->playerPos.y += speed.y;
 
 
-  draw_sprite(SPRITE_CELESTE_01, gameState->playerPos);
-  // draw_quad(gameState.playerPos, {50.0f, 50.0f});
-
   gameState->playerPos.y += 6.0f;
   if(gameState->playerPos.y > 500.0f - 4.0f * 17.0f)
   {
     gameState->playerPos.y = 500.0f - 4.0f * 17.0f;
     grounded = true;
   }
+
+  if(!grounded &&
+     gameState->playerPos.y < highestHeight)
+  {
+    highestHeight = gameState->playerPos.y;
+  }
+
+  draw_quad({0.0f, highestHeight + 2.0f}, {input->screenSize.x, 2.0});
+
+  draw_sprite(SPRITE_CELESTE_02, gameState->playerPos);
+  // draw_quad(gameState.playerPos, {50.0f, 50.0f});
 
   draw_quad({0.0f, 500.0f}, {input->screenSize.x / 2.0f, 10.0f});
   draw_quad({input->screenSize.x / 2.0f + 50.0f, 500.0f}, 
